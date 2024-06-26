a26c326601ce8c8ce051e0bc4787ef4cc91d1105
Astri Brugeilles <astri.brugeilles@gmail.com>
2024-06-26 15:11:58 +0200

    correction calculateFare() avec des millisecondes

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,11 +10,11 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        long inTime = ticket.getInTime().getTime();
+        long outTime = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        double duration = (double)(outTime - inTime) / (1000 * 60 * 60);
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
